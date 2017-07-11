29db5b8d5122dc4fb800a1a39e1337c42cd27a1e
lk235 <cool.lk235@cloud.com>
2017-07-11 17:37:46 +0200

    7111

--- kinghome.py
+++ kinghome.py
@@ -3,19 +3,11 @@ from bs4 import BeautifulSoup
 from urllib.error import HTTPError
 import re
 import datetime
-try:
+import requests
 
-    html = urlopen("http://www.kinghome.it/jking/index.php#")
+params = {'user':'c0000004812', 'pass':'abcd'}
+r = requests.post("http://www.kinghome.it/jking/index.php#",data=params)
+# html = urlopen("http://www.kinghome.it/jking/index.php#")
 
-except HTTPError as e:
-    print(e)
-if html is None:
-    print("URL is not found")
-else:
-    bsObj = BeautifulSoup(html, "html.parser")
-    # nameList = bsObj.findAll("span", {"class": {"a-size-base a-color-price s-price a-text-bold","a-size-small"}})
-    # for name in nameList:
-    #     print(name.get_text())
-    # namelist = bsObj.findAll(text="Domopak Sacchi Pattumiera Media - 15 Pezzi")
-    # print(len(namelist))
-    print(bsObj.prettify())
+
+print(r.text)
